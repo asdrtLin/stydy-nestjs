2b61bc1b461e1e4d72a6c0d3a61e4997004e1dc6
asdrtLin <86903002+asdrtLin@users.noreply.github.com>
2023-05-13 17:05:39 +0800

    feat:安装@nestjs/typeorm、typeorm 、mysql2包，连接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { GirlService } from './girl/girl.service';
 import { GirlController } from './girl/girl.controller';
 import { GirlModule } from './girl/girl.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [GirlModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'nest_js_study',
+      retryDelay: 500,
+      retryAttempts: 10,
+    }),
+    GirlModule,
+  ],
   controllers: [GirlController],
   providers: [GirlService],
 })
